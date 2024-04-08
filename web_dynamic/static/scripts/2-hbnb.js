d3a59d0e78ab4cc4ec5063045f8a8051f802df06
Abdellatif <abdelt.2012@gmail.com>
2024-04-08 21:03:24 +0000

    Done.

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -9,4 +9,12 @@ $(function () {
       }
       $('.amenities h4').text(Object.keys(amenities).sort().join(', '));
     });
+  // status
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
 });
